e1946ade38761019b4027f163a528464b0391334
HarutManukyan <102929456+HarutManukyan@users.noreply.github.com>
2022-07-16 01:21:15 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,162 @@
+let container = document.getElementById('container')
+let body = document.getElementById('body')
+let yes = document.getElementById('yes')
+let no = document.getElementById('no')
+
+function noFunc(){
+    let random = Math.round(Math.random() * 500)+'px'
+    container.style.margin = random
+}
+function yesFunc(){
+    container.remove()
+    let div = document.createElement('div')
+    div.classList.add('div')
+    div.append(`
+    I FUCKIN KNEW ITTTTTT!!!!!!!!!
+    `)
+    let img1 = document.createElement('img')
+    img1.src = 'https://c.tenor.com/kHcmsxlKHEAAAAAC/rock-one-eyebrow-raised-rock-staring.gif'
+    img1.classList.add('img')
+    setTimeout(interval =>{
+       
+        div.append(img1)
+    }, 2000)
+    let img2 = document.createElement('img')
+    img2.src = 'https://c.tenor.com/kHcmsxlKHEAAAAAC/rock-one-eyebrow-raised-rock-staring.gif'
+    img2.classList.add('img')
+    setTimeout(interval =>{
+        let random = Math.round(Math.random() * 700)+'px'
+        img2.style.position = 'absolute'
+        img2.style.top = random
+        body.append(img2)
+    }, 2200)
+    let img3 = document.createElement('img')
+    img3.src = 'https://c.tenor.com/kHcmsxlKHEAAAAAC/rock-one-eyebrow-raised-rock-staring.gif'
+    img3.classList.add('img')
+    setTimeout(interval =>{
+        let random = Math.round(Math.random() * 700)+'px'
+        img3.style.position = 'absolute'
+        img3.style.left = random
+        img14.style.top = '100px'
+        body.append(img3)
+    }, 2400)
+    let img4 = document.createElement('img')
+    img4.src = 'https://c.tenor.com/kHcmsxlKHEAAAAAC/rock-one-eyebrow-raised-rock-staring.gif'
+    img4.classList.add('img')
+    setTimeout(interval =>{
+        let random = Math.round(Math.random() * 700)+'px'
+        img4.style.position = 'absolute'
+        img4.style.top = random
+        img4.style.left = random
+        body.append(img4)
+    }, 2600)
+    let img5 = document.createElement('img')
+    img5.src = 'https://c.tenor.com/kHcmsxlKHEAAAAAC/rock-one-eyebrow-raised-rock-staring.gif'
+    img5.classList.add('img')
+    setTimeout(interval =>{
+        let random = Math.round(Math.random() * 1500)+'px'
+        img5.style.position = 'absolute'
+        img5.style.left = random
+        img14.style.top = '0'
+        body.append(img5)
+    }, 2800)
+    let img6 = document.createElement('img')
+    img6.src = 'https://c.tenor.com/kHcmsxlKHEAAAAAC/rock-one-eyebrow-raised-rock-staring.gif'
+    img6.classList.add('img')
+    setTimeout(interval =>{
+        let random = Math.round(Math.random() * 700)+'px'
+        img6.style.position = 'absolute'
+        img6.style.top = random
+        body.append(img6)
+    }, 3000)
+    let img7 = document.createElement('img')
+    img7.src = 'https://c.tenor.com/kHcmsxlKHEAAAAAC/rock-one-eyebrow-raised-rock-staring.gif'
+    img7.classList.add('img')
+    setTimeout(interval =>{
+        let random = Math.round(Math.random() * 700)+'px'
+        img7.style.position = 'absolute'
+        img7.style.top = random
+        img7.style.left = random
+        body.append(img7)
+    }, 3200)
+    let img8 = document.createElement('img')
+    img8.src = 'https://c.tenor.com/kHcmsxlKHEAAAAAC/rock-one-eyebrow-raised-rock-staring.gif'
+    img8.classList.add('img')
+    setTimeout(interval =>{
+        let random = Math.round(Math.random() * 600)+'px'
+        img8.style.position = 'absolute'
+        img8.style.top = random
+        body.append(img8)
+    }, 3400)
+    let img9 = document.createElement('img')
+    img9.src = 'https://c.tenor.com/kHcmsxlKHEAAAAAC/rock-one-eyebrow-raised-rock-staring.gif'
+    img9.classList.add('img')
+    setTimeout(interval =>{
+        let random = Math.round(Math.random() * 700)+'px'
+        img9.style.position = 'absolute'
+        img9.style.top = random
+        img9.style.left = random
+        body.append(img9)
+    }, 3600)
+    let img10 = document.createElement('img')
+    img10.src = 'https://c.tenor.com/kHcmsxlKHEAAAAAC/rock-one-eyebrow-raised-rock-staring.gif'
+    img10.classList.add('img')
+    setTimeout(interval =>{
+        let random = Math.round(Math.random() * 1500)+'px'
+        img10.style.position = 'absolute'
+        img10.style.left = random
+        img14.style.top = '0'
+        body.append(img10)
+    }, 3800)
+    let img11 = document.createElement('img')
+    img11.src = 'https://c.tenor.com/kHcmsxlKHEAAAAAC/rock-one-eyebrow-raised-rock-staring.gif'
+    img11.classList.add('img')
+    setTimeout(interval =>{
+        let random = Math.round(Math.random() * 600)+'px'
+        img11.style.position = 'absolute'
+        img11.style.top = random
+        body.append(img11)
+    }, 4000)
+    let img12 = document.createElement('img')
+    img12.src = 'https://c.tenor.com/kHcmsxlKHEAAAAAC/rock-one-eyebrow-raised-rock-staring.gif'
+    img12.classList.add('img')
+    setTimeout(interval =>{
+        let random = Math.round(Math.random() * 700)+'px'
+        img12.style.position = 'absolute'
+        img12.style.top = random
+        img12.style.left = random
+        body.append(img12)
+    }, 4200)
+    let img13 = document.createElement('img')
+    img13.src = 'https://c.tenor.com/kHcmsxlKHEAAAAAC/rock-one-eyebrow-raised-rock-staring.gif'
+    img13.classList.add('img')
+    setTimeout(interval =>{
+        let random = Math.round(Math.random() * 1000)+'px'
+        img13.style.position = 'absolute'
+        img13.style.left = random
+        img14.style.top = '0'
+        body.append(img13)
+    }, 4400)
+    let img14 = document.createElement('img')
+    img14.src = 'https://c.tenor.com/kHcmsxlKHEAAAAAC/rock-one-eyebrow-raised-rock-staring.gif'
+    img14.classList.add('img')
+    setTimeout(interval =>{
+        let random = Math.round(Math.random() * 1000)+'px'
+        img14.style.position = 'absolute'
+        img14.style.left = random
+        img14.style.top = '0'
+        body.append(img14)
+    }, 4600)
+    let img15 = document.createElement('img')
+    img15.src = 'https://c.tenor.com/kHcmsxlKHEAAAAAC/rock-one-eyebrow-raised-rock-staring.gif'
+    img15.classList.add('img')
+    setTimeout(interval =>{
+        let random = Math.round(Math.random() * 1000)+'px'
+        img15.style.position = 'absolute'
+        img15.style.left = random
+        img14.style.top = '0'
+        body.append(img15)
+    }, 4800)
+    body.append(div)
+
+}
